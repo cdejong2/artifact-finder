2715fbf0a51f07b44441d2debd92f300b82726e2
Cristina de Jong <cdejong2@jhu.edu>
2021-07-06 18:16:54 +0000

    added requests and SQL

---
+++ artifact-finder.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+
+location = input("Add a city: ")
+startYear = input('Add start year: ')
+endYear = input('Add end year: ')
+
+r = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/search?dateBegin='
+                 + startYear + '&dateEnd=' + endYear + '&q=' + location)
+j = r.json()
+print(j['total'])
+
+cols = ['title', 'objectName', 'artistDisplayName', 'period']
+df = pd.DataFrame(columns=cols)
+database_name = 'museum_objects'
+
+for id in j['objectIDs']:
+    obj = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+ str(id))
+    # df.loc[len(df.index)] = [obj['title'], obj['objectName'], obj['artistDisplayName'], obj['period']]
+
+    
+# os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+ database_name +';"')
+
+
+# engine = 'mysql://root:codio@localhost/museum_objects'
+# df.to_sql('objects', con=engine, if_exists='append', index=False)
