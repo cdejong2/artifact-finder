72d9ac2afff3bf6e7fce0a3af450c7914e076e70
Cristina de Jong <cdejong2@jhu.edu>
2021-07-08 20:10:32 +0000

    museum object testing

--- museumdata.sql
+++ museumdata.sql
@@ -36,17 +36,18 @@ CREATE TABLE `Boston` (
 
 LOCK TABLES `Boston` WRITE;
 /*!40000 ALTER TABLE `Boston` DISABLE KEYS */;
+INSERT INTO `Boston` VALUES ('The Crucifixion','Painting','Luca di Tommè di Nuto','');
 /*!40000 ALTER TABLE `Boston` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `New York`
+-- Table structure for table `New York City`
 --
 
-DROP TABLE IF EXISTS `New York`;
+DROP TABLE IF EXISTS `New York City`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `New York` (
+CREATE TABLE `New York City` (
   `title` text DEFAULT NULL,
   `objectName` text DEFAULT NULL,
   `artistDisplayName` text DEFAULT NULL,
@@ -55,23 +56,22 @@ CREATE TABLE `New York` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `New York`
+-- Dumping data for table `New York City`
 --
 
-LOCK TABLES `New York` WRITE;
-/*!40000 ALTER TABLE `New York` DISABLE KEYS */;
-INSERT INTO `New York` VALUES ('The Little Fourteen-Year-Old Dancer','Statue','Edgar Degas',''),('Plate','Plate','Herman A. Kähler',''),('Ashtray','Ashtray','Herman A. Kähler',''),('Vase with lid','Vase with lid','Herman A. Kähler',''),('The Crucifixion','Painting','Luca di Tommè di Nuto','');
-/*!40000 ALTER TABLE `New York` ENABLE KEYS */;
+LOCK TABLES `New York City` WRITE;
+/*!40000 ALTER TABLE `New York City` DISABLE KEYS */;
+/*!40000 ALTER TABLE `New York City` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `london`
+-- Table structure for table `Philadelphia`
 --
 
-DROP TABLE IF EXISTS `london`;
+DROP TABLE IF EXISTS `Philadelphia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `london` (
+CREATE TABLE `Philadelphia` (
   `title` text DEFAULT NULL,
   `objectName` text DEFAULT NULL,
   `artistDisplayName` text DEFAULT NULL,
@@ -80,13 +80,85 @@ CREATE TABLE `london` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `london`
+-- Dumping data for table `Philadelphia`
 --
 
-LOCK TABLES `london` WRITE;
-/*!40000 ALTER TABLE `london` DISABLE KEYS */;
-INSERT INTO `london` VALUES ('Ecce Homo / Man of Sorrows','Print','Hans Schäufelein','');
-/*!40000 ALTER TABLE `london` ENABLE KEYS */;
+LOCK TABLES `Philadelphia` WRITE;
+/*!40000 ALTER TABLE `Philadelphia` DISABLE KEYS */;
+INSERT INTO `Philadelphia` VALUES ('The Thinker: Portrait of Louis N. Kenton','Painting','Thomas Eakins',''),('Signora Gomez d\'Arza','Painting','Thomas Eakins',''),('Mrs. Mary Arthur','Painting','Thomas Eakins',''),('Library Table','Library table','Rose Valley Shops',''),('Stained Glass Window','Stained glass window','William Lightfoot Price',''),('Stained Glass Window','Stained glass window','William Lightfoot Price',''),('Stained Glass Window','Stained glass window','William Lightfoot Price',''),('Stained Glass Window','Stained glass window','William Lightfoot Price',''),('New Jersey Beach','Painting','William Trost Richards',''),('The Lincoln Centenary','Medal','George T. Morgan',''),('Variety Act 11: Mac Bull from Philadelphia in His Frightful Loop-the-Loop Ride in His Car (Varietenummer 11: Mac Bull aus Philademphia in Seiner Fürchterlichen Looping the Loop Fahrt im Automobil)','Print','Moriz Jung',''),('Gustav Mahler','Sculpture','Auguste Rodin',''),('Mont Sainte-Victoire','Painting','Paul Cézanne',''),('Two Women','Painting','Paul Gauguin',''),('Still-Life with a Watermelon and Pomegranates','Drawing','Paul Cézanne',''),('Landscape','Drawing','Henri-Edmond Cross (Henri-Edmond Delacroix)',''),('Young Woman Reading (Ines)','Drawing','Umberto Boccioni',''),('Self-Portrait','Painting','Umberto Boccioni',''),('Sheet of Studies','Drawing','Umberto Boccioni',''),('Study for \"Homage to Mother\"','Drawing','Umberto Boccioni','');
+/*!40000 ALTER TABLE `Philadelphia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Venice`
+--
+
+DROP TABLE IF EXISTS `Venice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Venice` (
+  `title` text DEFAULT NULL,
+  `objectName` text DEFAULT NULL,
+  `artistDisplayName` text DEFAULT NULL,
+  `period` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Venice`
+--
+
+LOCK TABLES `Venice` WRITE;
+/*!40000 ALTER TABLE `Venice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Venice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `berlin`
+--
+
+DROP TABLE IF EXISTS `berlin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `berlin` (
+  `title` text DEFAULT NULL,
+  `objectName` text DEFAULT NULL,
+  `artistDisplayName` text DEFAULT NULL,
+  `period` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `berlin`
+--
+
+LOCK TABLES `berlin` WRITE;
+/*!40000 ALTER TABLE `berlin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `berlin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `boston`
+--
+
+DROP TABLE IF EXISTS `boston`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `boston` (
+  `title` text DEFAULT NULL,
+  `objectName` text DEFAULT NULL,
+  `artistDisplayName` text DEFAULT NULL,
+  `period` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boston`
+--
+
+LOCK TABLES `boston` WRITE;
+/*!40000 ALTER TABLE `boston` DISABLE KEYS */;
+/*!40000 ALTER TABLE `boston` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -110,7 +182,7 @@ CREATE TABLE `objects` (
 
 LOCK TABLES `objects` WRITE;
 /*!40000 ALTER TABLE `objects` DISABLE KEYS */;
-INSERT INTO `objects` VALUES ('View over Hallingdal','Painting','Johan Christian Dahl',''),('Mother and Child by the Sea','Painting','Johan Christian Dahl',''),('Peg tankard','Peg tankard','Isack Andersen Feldthus',''),('The River Elbe in Moonlight','Drawing','Johan Christian Dahl',''),('A View of Lower Rydal Falls, Cumbria','Drawing','Thomas Fearnley',''),('The Labro Falls','Drawing','Thomas Fearnley',''),('Shipwreck on a Rocky Shore','Print','Johan Christian Dahl',''),('Panoramic landscape near Bergen','Drawing','Monogrammist DS',''),('Portrait of Juliane Sophie Heger as a Child, leaning on a Table','Drawing','Johan Christian Dahl',''),('Portrait of Eline Marie Heger as a Child, Leaning on a Table, Looking at a Book','Drawing','Johan Christian Dahl',''),('Copenhagen Harbor by Moonlight','Painting','Johan Christian Dahl',''),('Political Chemist and German Retorts or Dissolving the Rhenish Confederacy','Print','Thomas Rowlandson',''),('Two Men before a Waterfall at Sunset','Painting','Johan Christian Dahl',''),('An Eruption of Vesuvius','Painting','Johan Christian Dahl',''),('Coffee spoon','Coffee spoon','Hans Bjerregaard Steen',''),('The North Cape by Moonlight','','Peder Balke',''),('Study of Water and Plants','Painting','Thomas Fearnley',''),('Gudvangen','Painting','Thomas Fearnley',''),('The Harbour of Copenhagen from the Esplanade between Langelinie and Toldboden; verso: Sketch of a Standing Man','Drawing','Christoffer Wilhelm Eckersberg',''),('Arthur Hartmann','Medallion','Max Klein',''),('Self-Portrait','Drawing','Umberto Boccioni',''),('Galatea','Statuette','Max Klinger',''),('Seven Trees','Drawing','Paula Modersohn-Becker',''),('George Inness Gold Medal','Plaque','Jonathan Scott Hartley',''),('Landscape','Painting','Wilhelm Trübner',''),('Still-Life with a Watermelon and Pomegranates','Drawing','Paul Cézanne','');
+INSERT INTO `objects` VALUES ('Mandolino','Mandolino','',''),('Design Wall Elevation with Stucco and Painted Decorations','Drawing Ornament & Architecture','Luigi Garzi',''),('Design Wall Elevation with Stucco and Painted Decorations','Drawing Ornament & Architecture','Luigi Garzi',''),('Curb Bit of Johannes Philip of Lamberg, Bishop of Passau (1651-1712)','Bit','',''),('Standing man with his right hand resting on a basin, shown in three-quarters view with his head turned toward the left, from the series \'Figures of fashion\' (Figures de modes)','Print','Antoine Watteau',''),('Man walking and carrying a cane in his right hand, shown in three-quarters view with his head turned away from the viewer, from the series \'Figures of fashion\' (Figures de modes)','Print','Antoine Watteau',''),('Nouveau Theatre de la Grande Bretagne: Ou Description Exacte des Palais de La Reine, et des Maisons les plus considerables...de la Grande Bretagne','Books Prints Ornament & Architecture','Johannes Kip','');
 /*!40000 ALTER TABLE `objects` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -123,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-07 20:23:13
+-- Dump completed on 2021-07-08 19:05:38
